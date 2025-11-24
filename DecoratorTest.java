424bfe5ab490a6291936f6eb2ba7a3d209c75368
MNSE <alstj1030@gmail.com>
2025-11-25 07:05:52 +0900

    P1P2

--- DecoratorTest.java
+++ DecoratorTest.java
@@ -2,15 +2,13 @@ import java.io.*;
 
 public class DecoratorTest {
     public static void main(String[] args) throws Exception {
-        // 암호화해서 파일에 쓰기
-        try (Writer w = new EncryptWriter(
-                new BufferedWriter(new FileWriter("encrypted_p1.txt")))) {
+        EncryptionStrategy caesar = new CaesarEncryptionStrategy();
+
+        try (Writer w = new EncryptWriter(new BufferedWriter(new FileWriter("encrypted_p1.txt")), caesar)) {
             w.write("Hello World!");
         }
 
-        // 복호화하면서 읽기
-        try (Reader r = new DecryptReader(
-                new BufferedReader(new FileReader("encrypted_p1.txt")))) {
+        try (Reader r = new DecryptReader(new BufferedReader(new FileReader("encrypted_p1.txt")), caesar)) {
             int ch;
             while ((ch = r.read()) != -1) {
                 System.out.print((char) ch);
